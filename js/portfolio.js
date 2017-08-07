8e1f09dc2cd1fca6ded7e8b4850ce869803cfb61
Yulia Seredich <yseredich@gmail.com>
2017-08-07 20:09:32 +0300

    add loop mode and fade

--- js/portfolio.js
+++ js/portfolio.js
@@ -2,7 +2,12 @@
     var galleryTop = new Swiper('.gallery-top', {
         nextButton: '.swiper-button-next',
         prevButton: '.swiper-button-prev',
-        spaceBetween: 10
+        spaceBetween: 10,
+        effect: 'fade',
+        loopedSlides: document.querySelectorAll('.gallery-top .swiper-slide').length,
+        loop: true,
+        preloadImages: false,
+        lazyLoading: true
     });
 
     var galleryThumbs = new Swiper('.gallery-thumbs', {
@@ -11,7 +16,9 @@
         slidesPerView: 'auto',
         touchRatio: 0.2,
         slideToClickedSlide: true,
-        autoplay: 10000
+        autoplay: 10000,
+        loopedSlides: document.querySelectorAll('.gallery-thumbs .swiper-slide').length,
+        loop: true
     });
 
     galleryTop.params.control = galleryThumbs;
