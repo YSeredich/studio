67903018f0952e208a0353b8ac7aa59df18e887a
Yulia Seredich <yseredich@gmail.com>
2017-09-02 03:45:10 +0300

    text and optimization

--- js/about.js
+++ js/about.js
@@ -2,7 +2,7 @@ var about = function() {
     if (Math.random() >= 0.5) {
         var img = document.querySelector('.about__image');
         if (img) {
-            img.setAttribute('src', 'https://ucarecdn.com/a6f679f5-d042-410d-8da8-62829cc65f3d/-/progressive/yes/-/quality/lighter/office_2.jpg');
+            img.setAttribute('src', 'https://ucarecdn.com/a6f679f5-d042-410d-8da8-62829cc65f3d/-/resize/800x/-/progressive/yes/-/quality/lighter/office_2.jpg');
         }
     }
 }();
